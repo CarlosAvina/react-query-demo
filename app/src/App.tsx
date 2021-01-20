4a27b618e514ced27731e7493af4e0f3c4585483
Carlos Avina <carlos.avina@cbqasolutions.com>
2021-01-19 23:01:30 -0600

    Set routing

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+
+import NotesPage from "./screens/NotesPage";
+import EditNotePage from "./screens/EditNotePage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <NotesPage />
+          </Route>
+          <Route path="/edit">
+            <EditNotePage />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
